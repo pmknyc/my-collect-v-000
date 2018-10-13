c9f971f5afea2a51ae2166e48a7af65e2418c4d1
Paula McKinley <pmknyc@optimum.net>
2018-10-13 00:06:55 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
+# code a method to behave
+# like the #collect iterator method
 
-
+def my_collect(array)
+  i = 0
+  collection = []
+  while i < array.length
+    collection << yield(array[i])
+    i += 1
+  end
+  collection
+end
